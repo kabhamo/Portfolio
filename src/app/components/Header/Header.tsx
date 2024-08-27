b8122ff06734d16425ce02c924f6bdcd15d033df
kabha <mkabha54@gmail.com>
2024-08-28 01:00:54 +0300

    add: styles tailwind

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import Nav from '../Nav/Nav'
+import MobileNav from '../MobileNav/MobileNav'
 
 const Header = () => {
     return (
@@ -21,7 +22,9 @@ const Header = () => {
                     </Link>
                 </div>
                 {/* mobile nav */}
-                <div className="xl:hidden">mobile nav</div>
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
             </div>
         </header>
     )
--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,32 +1,10 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-
-const links = [
-  {
-    name: 'home',
-    path: '/',
-  },
-  {
-    name: 'services',
-    path: '/services',
-  },
-  {
-    name: 'resume',
-    path: '/resume',
-  },
-  {
-    name: 'work',
-    path: '/work',
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-  },
-]
+import { links } from '@/app/utils/constants'
 
 const Nav = () => {
-  const pathname = usePathname()
+  const pathName = usePathname()
   return (
     <nav className='flex gap-8'>
       {links.map((link, index) => {
@@ -34,7 +12,7 @@ const Nav = () => {
           <Link
             href={link.path}
             key={index}
-            className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
+            className={`${link.path === pathName && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
             {link.name}
           </Link>)
       })}
