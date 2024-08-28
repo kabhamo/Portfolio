7a6fceb9a3136c386bd18d7fc5b881f997c34c02
kabha <mkabha54@gmail.com>
2024-08-28 17:48:17 +0300

    add: profile photo

--- src/app/components/Photo/Photo.tsx
+++ src/app/components/Photo/Photo.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 const Photo = () => {
     return (
         <div className="w-full h-full relative">
+
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{
@@ -13,6 +14,7 @@ const Photo = () => {
                     transition: { delay: 1.6, duration: 0.3, ease: 'easeIn' },
                 }}
             >
+                {/* image */}
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{
@@ -23,10 +25,10 @@ const Photo = () => {
                             ease: 'easeInOut',
                         },
                     }}
-                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+                    className="w-[298px] h-[298px] xl:w-[490px] xl:h-[490px] mix-blend-lighten absolute"
                 >
                     <Image
-                        src="/assets/photo.jpg"
+                        src="/assets/cropped_image.png"
                         priority
                         quality={100}
                         fill
@@ -34,6 +36,34 @@ const Photo = () => {
                         className="object-contain"
                     />
                 </motion.div>
+
+                {/* circle */}
+                <motion.svg
+                    className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+                    fill="transparent"
+                    viewBox="0 0 506 506"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <motion.circle
+                        cx="253"
+                        cy="253"
+                        r="250"
+                        stroke="#00ff99"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        initial={{ strokeDasharray: "24 10 0 0" }}
+                        animate={{
+                            strokeDasharray: ["15 120 25 2", "16 25 92 72", "4 250 22 22"],
+                            rotate: [120, 360]
+                        }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            repeatType: "reverse"
+                        }}
+                    />
+                </motion.svg>
             </motion.div>
         </div>
     )
