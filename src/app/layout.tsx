f42e38160884d458614c5746d03dd1e830cb05e9
kabha <mkabha54@gmail.com>
2024-08-30 23:09:46 +0300

    add: speed-insights pkg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
@@ -26,7 +27,10 @@ export default function RootLayout({
             <body className={JetBrainsMono.variable}>
                 <Header />
                 <StairTransition />
-                <PageTransition>{children}</PageTransition>
+                <PageTransition>
+                    {children}
+                    <SpeedInsights />
+                </PageTransition>
             </body>
         </html>
     )
