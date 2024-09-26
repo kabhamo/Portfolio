ed625926ba477ae83e32844d20877c1861eb41a6
kabha <mkabha54@gmail.com>
2024-09-26 11:02:04 +0300

    add: new cv file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const Home = () => {
                         </p>
                         {/* btn and socials */}
                         <div className="flex flex-col xl:flex-row items-center gap-8">
-                            <a href="/assets/resume/MohamedKabha.pdf" download>
+                            <a href="/assets/resume/Mohamed_Kabha.pdf" download>
                                 <Button
                                     variant="outline"
                                     size="lg"
